cc3937306b01041100955c9eb0a4ca8927f3a69f
rufunt <jazzoo@mail.ru>
2018-10-03 14:36:22 +0000

    Add Edit? Update and Delete actions

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -23,12 +23,19 @@ class PlaysController < ApplicationController
   end
   
   def update
+    if @play.update(play_params)
+      redirect_to play_path(@play)
+    else
+      render 'edit'
+    end
   end
   
   def edit
   end
   
   def destroy
+    @play.destroy
+    redirect_to root_path
   end
   
   private

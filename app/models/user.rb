8056d75140bca596b5b4f1d19f2fbc8cee8c9b7f
rufunt <jazzoo@mail.ru>
2018-10-04 11:58:06 +0000

    Add associations betwen models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :plays
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
 end

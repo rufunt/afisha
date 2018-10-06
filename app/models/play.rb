60e4e4770abf651039de6f7c22a3969d20b241e5
rufunt <jazzoo@mail.ru>
2018-10-06 13:03:07 +0000

    Add Paperclip gem and change view files

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,4 +1,8 @@
 class Play < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+  include Paperclip::Glue
+  
+  has_attached_file :play_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
 end
